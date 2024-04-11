489031e2290c96a69939263f0f06fb749b01fc50
Daniel Kiyiki <danielngolu154@gmail.com>
2024-04-11 19:34:52 +0200

    MakingTheValueCheckedForZeroENtry

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
 
-	if (value == NULL)
+	if (value == 0)
 		return (NULL);
 
 	if (parent == NULL || newnode == NULL)
