36155bd6accc8afc4ad898e66c645d9ef21e71d1
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-04-12 00:46:14 +0200

    fixing betty on task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,7 +13,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	size_t depth = 0;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	while (tree->parent != NULL)
 	{
@@ -21,6 +21,6 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		tree = tree->parent;
 	}
 
-	return depth;
+	return (depth);
 }
 
