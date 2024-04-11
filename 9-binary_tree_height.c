2b88482200ec1775088fd02123c0242a08117a70
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-04-12 00:51:47 +0200

    q9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,10 +9,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-		return (0);
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	size_t left_height, right_height;
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+    if (tree == NULL)
+        return 0;
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    if (left_height > right_height)
+        return (left_height + 1);
+    else
+        return (right_height + 1);
 }
