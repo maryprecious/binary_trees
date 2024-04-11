5e661187145b3fb882c56cdcc1c9484a04700163
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-04-12 01:01:39 +0200

    cleared the code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,15 +10,15 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newnode;
 
-	if (parent == NULL || newnode == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	newnode->left = NULL;
-	newnode->right = NULL;
-	newnode->n = value;
-	newnode->parent = parent;
+	newnode = binary_tree_node(parent, value);
+
+	if (newnode == NULL)
+		return (NULL);
 
 	if (parent->left == NULL)
 	{
