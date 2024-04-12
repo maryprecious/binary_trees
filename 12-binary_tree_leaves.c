a8b0fa4bde6c9e32bd4e7bc20b5134c298aef8c7
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-04-12 02:38:18 +0200

    number 12 trying

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,21 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - function that counts leaves of a binary tree
- * @tree:  The pointer to node to count leaves
- * Description: 12. Leaves
- * Return: see below
- * 1. upon success, return number of leaves
- * 2. upon fail, return 0
+ * binary_tree_height - get height node
+ * @tree: pointer to the root node to traverse
+ * Return: return the newnode or NULL
  */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	/* declare and initialize variable for leaves */
-	size_t leaves = 0;
-
-	/* base case */
 	if (tree == NULL)
-	{
 		return (0);
+	else
+	{
+		size_t left_height = binary_tree_height(tree->left);
+		size_t right_height = binary_tree_height(tree->right);
+
+		return (1 + (left_height - 1 > right_height - 1 ? left_height : right_height));
 	}
+
 }
